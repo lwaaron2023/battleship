c0bbad3e2627998402e6784bca2d057f8d9580d9
lwaaron2023 <alwaller@wpi.edu>
2025-10-03 22:15:14 -0400

    Began creating the server to host the game

---
+++ serverSide/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import http from 'http'
+import ViteExpress from 'vite-express'
+import { WebSocketServer } from 'ws'
+
+const app = express()
+app.use(express.static('public'))
+
+
+const server = http.createServer( app ),
+    socketServer = new WebSocketServer({ server }),
+    clients = []
+
+    socketServer.on( 'connection', client => {
+        client.groupNum = clients.length>>>1;
+        client.id = clients.length;
+        // console.log( 'connection',client)
+        client.on( 'message', msg => {
+            console.log(client.id,client.groupNum,msg)
+        })
+        // add client to client list
+        clients.push(client)
+    })
+
+server.listen( 3000 || process.env.PORT)
+
+ViteExpress.bind( app, server )
